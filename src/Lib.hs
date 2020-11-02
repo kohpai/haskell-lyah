429a9f1b39fd8753a69801d5cc21b9080ef2fd58
Kohpai <whcwhc78@gmail.com>
2020-11-02 23:46:21 +0100

    :sparkles: day 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,12 +4,16 @@ module Lib
   )
 where
 
+import Data.Map as Map
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-data Point = Point Float Float
+-- concrete types defined with data keyword
+data Point = Point Float Float deriving (Show)
 
-data Shape = Circle Point Float | Rectangle Point Point
+-- Circle and Rectangle are type constructors
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
 surface :: Shape -> Float
 surface (Circle _ r) = pi * r ^ 2
@@ -20,7 +24,7 @@ data Person = Person
     lastName :: String,
     age :: Int
   }
-  deriving (Show)
+  deriving (Show, Eq)
 
 data Vector a = Vector a a a deriving (Show)
 
@@ -32,3 +36,48 @@ vectMult :: (Num t) => Vector t -> t -> Vector t
 
 scalarMult :: (Num t) => Vector t -> Vector t -> Vector t
 (Vector i j k) `scalarMult` (Vector l m n) = Vector (i * l) (j * m) (k * n)
+
+-- type synonyms defined with type keyword
+
+type PhoneNumber = String
+
+type Name = String
+
+type PhoneBook = [(Name, PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name number book = (name, number) `elem` book
+
+-- type constructor that takes two types and produces a concrete type
+type AssocList k v = [(k, v)]
+
+-- partially applied type constructor
+type IntMap = Map.Map Int
+
+restrictedNull :: IntMap String -> Bool
+restrictedNull v = Map.null v
+
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+-- use Left for error string and Right for result
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup number map =
+  case Map.lookup number map of
+    Nothing -> Left $ "Locker number " ++ show number ++ " does not exist."
+    Just (state, code) ->
+      if state /= Taken
+        then Right code
+        else Left $ "Locker " ++ show number ++ " is already taken"
+
+hasError :: Either String Code -> Bool
+hasError v =
+  case v of
+    Left _ -> True
+    Right _ -> False
+
+-- deriving Show is necessary for displaying in ghci
+data List a = Empty | Cons a (List a) deriving (Show)
