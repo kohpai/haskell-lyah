81974f32cbfc5f04abee5ed2c78f6654af393e7e
Kohpai <whcwhc78@gmail.com>
2022-05-28 23:12:09 +0200

    :memo: binding <-

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,15 @@ module Main where
 --import Lib
 
 main :: IO ()
-main = do
+main = simpleMain
+
+simpleMain :: IO ()
+-- do contains sequence of IO actions
+-- the last action cannot be bound (<-)
+simpleMain = do
   putStrLn "Hello, what's your name?"
   -- getLine returns IO String
-  -- to make it into String, <- is the only way
+  -- to make it into String, binding (<-) is the only way
   -- <- can be used only in a function with IO action (f :: IO something)
   name <- getLine
   putStrLn $ "Hey " ++ name ++ ", you rock!"
