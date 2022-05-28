52becbd7aab95f3bb4b19124b2e8fecf9b1e11a2
Kohpai <whcwhc78@gmail.com>
2022-05-28 22:52:21 +0200

    :sparkles: IO

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,10 @@ module Main where
 --import Lib
 
 main :: IO ()
-main = putStrLn "Hello Worlds!"
+main = do
+  putStrLn "Hello, what's your name?"
+  -- getLine returns IO String
+  -- to make it into String, <- is the only way
+  -- <- can be used only in a function with IO action (f :: IO something)
+  name <- getLine
+  putStrLn $ "Hey " ++ name ++ ", you rock!"
